cab07b7bb854f65f5f2d01c464c2c70112f774a2
LilianeFalcao <lilianefalcao2293@gmail.com>
2025-10-10 15:39:58 -0300

    Aula do dia 10/10/2025

---
+++ Exerc_1.py
@@ -0,0 +1,65 @@
+"""
+Exercício 1:
+
+Dado um array (arr) desordenado de inteiros distintos, faça a ordenação do mesmo utilizando o algoritmo Quicksort apresentado em sala (Slides já disponibilizados).
+
+Em seguida, com o vetor já ordenado e um valor "alvo", retorne o índice se o "alvo" for encontrado. Caso contrário, retorne o índice onde ele deveria ser inserido de forma a manter a ordem do array.
+
+a) Faça o exercício utilizando Busca Linear.
+b) Faça o exercício utilizando utilizando Busca Binária. 
+
+OBS.:
+PODE FAZER TODO EXERCÍCIO 1 EM UM ÚNICO ARQUIVO.
+(Lembre-se de fazer a implementação o quicksort, a busca linear para inserção e a busca binária para inserção).
+
+## 1- usa o quick_sort pra ordenar o vetor e depois e binária
+ 
+"""
+
+##A)
+def buscaLinear(arr, target):
+    print (arr)
+    for e in range(len(arr)):
+        if arr[e] == target:
+            return e   
+    return "não encontrado"
+        
+arr = [93, 67, 4, 8, 91, 76, 95, 1, 63, 96]
+target = 1
+
+print("Target encontrado na posição: ",buscaLinear(arr, target))
+
+##B)
+
+def quicksort(arr):
+
+    _quicksort(arr, 0, len(arr) - 1)
+
+def _quicksort(arr, left, right):
+    
+    if left < right:  
+        pi = partition(arr, left, right)  
+        
+        _quicksort(arr, left, pi - 1)
+        
+        _quicksort(arr, pi + 1, right)
+
+def partition(arr, left, right):
+   
+    pivot = arr[right]  
+    
+    i = left - 1  
+
+    for e in range(left, right):
+        if arr[e] <= pivot:
+            i = i+1
+
+            arr[i], arr[e] = arr[e], arr[i]
+    arr [i+1], arr[right], = arr[right], arr[i+1]
+    return i+1
+
+def buscaB(arr, target):
+
+    quicksort(arr)
+
+    
---
+++ Exerc_2.py
@@ -0,0 +1,28 @@
+"""
+Exercício 2:
+
+Dado um array de inteiros chamado citações, onde cada elemento é o número de citações que um artigo de um determinado pesquisador recebeu e o array contém todos os artigos desse pesquisador, determine qual o índice-h dessa pessoa.  
+
+O array citações e entregue ordenado do MAIOR para o MENOR (Decrescente).
+
+O índice-h é definido como o valor máximo de "h" tal que o pesquisador em questão publicou pelo menos "h" artigos, cada um dos quais foi citado pelo menos "h" vezes.
+
+a) Faça o exercício utilizando Busca Linear.
+b) Faça o exercício utilizando utilizando Busca Binária. 
+
+PODE FAZER TODO EXERCÍCIO 2 EM UM ÚNICO ARQUIVO. 
+
+Pode ser uma função h_index_linear(citacoes) e outra h_index_binaria(citacoes)
+
+##2- recebe um array de citações, decrescente, calcular o index-h  
+
+"""
+
+
+import random
+
+# Lista de 10 números desordenados
+numeros = random.sample(range(1, 101), 10)
+print(numeros)
+
+
